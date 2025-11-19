14de3ed217028d33726a384b20af92234bba541b
Morkor98 <miki.marcucci@gmail.com>
2025-11-19 09:38:08 +0100

    first commit

---
+++ WebAPIActors/Controllers/ActorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIActors.Helper;
+using WebAPIActors.Models;
+
+namespace WebAPIActors.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActorController : Controller
+    {
+        [HttpGet(Name = "GetAllActors")]
+        public List<Actor> GetAllActors()
+        {
+            return DatabaseHelper.GetAllActors();
+        }
+
+        [HttpGet("{id}", Name = "GetOneActor")]
+        public ActionResult<Actor> GetActor(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Id non valido. Deve essere > 0");
+            
+            var actor = DatabaseHelper.GetActorById(id);
+
+            if(actor == null)
+                return NotFound();
+
+            return actor;
+        }
+        [HttpGet("research", Name ="FindActor")]
+        public ActionResult<List<Actor>> FindActor(string research)
+        {
+            if (research == null)
+                return BadRequest("Inserisci un dato");
+
+            var actors = DatabaseHelper.FindActor(research);
+
+            if(actors== null || actors.Count == 0)
+                return NotFound();
+            return actors;
+        }
+        [HttpPost("new", Name = "AddActor")]
+        public ActionResult<int> AddActor(Actor actor)
+        {
+            if (actor == null ||
+                string.IsNullOrEmpty(actor.Name) ||
+                string.IsNullOrEmpty(actor.Surname) ||
+                string.IsNullOrEmpty(actor.ImgUrl) ||
+                string.IsNullOrEmpty(actor.Nation))
+                return BadRequest("I campi contrasegnati da * sono obbligatori");
+            if (actor.FilmCachet <= 0)
+                return BadRequest("Il vaolre del cachet non può essere negativo");
+            if (actor.FilmQuantity <= 0)
+                return BadRequest("Non può aver fatto meno di 0 film");
+
+            return DatabaseHelper.Insert(actor);
+        }
+    }
+}
---
+++ WebAPIActors/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPIActors.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nation { get; set; }
+        public int FilmQuantity { get; set; }
+        public double FilmCachet { get; set; }
+        public string ImgUrl { get; set; }
+
+    }
+}
